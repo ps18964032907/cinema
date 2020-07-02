46798d9263d28a904346b4f4c8b3e6130e783212
mhd <mhd@qq.com>
2020-07-02 12:06:03 +0800

    后台+brand

--- src/main/java/com/pmsj/cinema/system/controller/BrandController.java
+++ src/main/java/com/pmsj/cinema/system/controller/BrandController.java
@@ -5,7 +5,7 @@ import com.pmsj.cinema.common.entity.Brand;
 import com.pmsj.cinema.system.exception.NullParametersException;
 import com.pmsj.cinema.system.exception.PageInfoErrorException;
 import com.pmsj.cinema.system.service.BrandService;
-import com.pmsj.cinema.system.uitl.ReturnUtil;
+import com.pmsj.cinema.system.util.ReturnUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
