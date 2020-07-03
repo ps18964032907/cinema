1ef19bfa716ca8dbcb2ccdf3bc773a8d05b19df2
sjh <742673570@qq.com>
2020-07-03 15:29:39 +0800

    电影类别 sjh

--- src/main/java/com/pmsj/cinema/business/controller/MovieDetailsController.java
+++ src/main/java/com/pmsj/cinema/business/controller/MovieDetailsController.java
@@ -2,11 +2,15 @@ package com.pmsj.cinema.business.controller;
 
 import com.pmsj.cinema.business.service.MovieDetailsService;
 import com.pmsj.cinema.common.entity.Movie;
+import com.pmsj.cinema.common.entity.MovieTpye;
 import com.pmsj.cinema.common.util.MovieDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.plaf.PanelUI;
+import java.util.List;
 
 /**
  * @Author sjh
@@ -18,9 +22,13 @@ public class MovieDetailsController {
     @Autowired
     MovieDetailsService movieDetailsService;
 
-    @RequestMapping("indexMovieDetails/{movieId}")
+    @RequestMapping("/indexMovieDetails/{movieId}")
     public Movie selectByPrimaryKey(@PathVariable Integer movieId) {
         Movie movie = movieDetailsService.selectByPrimaryKey(movieId);
         return movie;
+    }
+    @RequestMapping("/movieType/{movieId}")
+    public List<MovieTpye> selectByMovieId(@PathVariable Integer movieId){
+        return movieDetailsService.selectByMovieId(movieId);
     }
 }
