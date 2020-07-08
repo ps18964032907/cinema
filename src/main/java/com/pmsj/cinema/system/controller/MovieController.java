8815c2c8c8bb2ef8c2ff21f8a2ad2c957927e0d5
2286010407@qq.com <2286010407@qq.com>
2020-07-08 15:17:02 +0800

    ddd

--- src/main/java/com/pmsj/cinema/system/controller/MovieController.java
+++ src/main/java/com/pmsj/cinema/system/controller/MovieController.java
@@ -5,20 +5,31 @@ import com.pmsj.cinema.business.service.MovieActorService;
 import com.pmsj.cinema.common.entity.Cast;
 import com.pmsj.cinema.common.entity.Movie;
 import com.pmsj.cinema.common.entity.MovieTpye;
+import com.pmsj.cinema.common.entity.Result;
 import com.pmsj.cinema.common.mapper.MovieMapper;
 import com.pmsj.cinema.common.vo.CastVo;
 import com.pmsj.cinema.common.vo.CinemaVo;
 import com.pmsj.cinema.system.service.MovieService;
 import com.pmsj.cinema.system.util.ReturnUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletContext;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 /*
  * @Author 潘升
@@ -51,14 +62,61 @@ public class MovieController {
 
     @RequestMapping("addMovie")
     @ResponseBody
-    public void addMovie(Movie movie, String[] typeId, String[] actorRole, String[] actorType, String[] castId, MultipartFile movieImg) {
+    public Result addMovie(HttpServletRequest request, Movie movie, String[] movieTypes, String[] cast0, String[] cast1, String[] cast1Role, @RequestParam(value = "image", required = false) MultipartFile image) throws IOException {
 
         System.out.println(movie);
-        System.out.println(Arrays.toString(typeId));
-        System.out.println(Arrays.toString(actorRole));
-        System.out.println(Arrays.toString(actorType));
 
-        System.out.println(Arrays.toString(castId));
+        movie.setMovieTime(movie.getMovieTime() + "分钟");
 
+        if (image == null) {
+            return new Result<>().error("图片为空", null);
+        }
+
+
+        //判断用户是否选择图片
+        if (image.getOriginalFilename() == null || image.getOriginalFilename().length() <= 0) {
+        } else {
+//        image
+            String fileName = image.getOriginalFilename();
+            String substring = "";
+            if (fileName.lastIndexOf(".") != -1) {
+                substring = fileName.substring(fileName.lastIndexOf("."));
+            }
+            String name = UUID.randomUUID().toString().replaceAll("-", "") + substring;
+            request.getServletPath();
+
+            String property = System.getProperty("user.dir");
+            String realPath = property + "\\src\\main\\resources\\static\\img";
+
+            movie.setMovieImg("/img/" + name);
+            System.out.println(realPath);
+//            image.getInputStream. (realPath + "/" + name);
+//            goods.setImage("/images/goods/" + name);
+            try {
+                File fileMkdir = new File(realPath);
+
+                if (!fileMkdir.exists()) {
+                    fileMkdir.mkdir();
+                }
+                //定义输出流 将文件保存在D盘    file.getOriginalFilename()为获得文件的名字
+                FileOutputStream os = new FileOutputStream(fileMkdir.getPath() + "\\" + name);
+                InputStream in = image.getInputStream();
+                int b = 0;
+                while ((b = in.read()) != -1) { //读取文件
+                    os.write(b);
+                }
+                os.flush(); //关闭流
+                in.close();
+                os.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                ;
+                System.out.println("上传失败！");
+            }
+        }
+
+        movieService.addMovie(movie, movieTypes, cast0, cast1, cast1Role);
+
+        return null;
     }
 }
--- src/main/java/com/pmsj/cinema/system/service/MovieService.java
+++ src/main/java/com/pmsj/cinema/system/service/MovieService.java
@@ -3,9 +3,14 @@ package com.pmsj.cinema.system.service;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.pmsj.cinema.common.entity.Movie;
+import com.pmsj.cinema.common.entity.MovieActor;
+import com.pmsj.cinema.common.entity.MovieTpye;
+import com.pmsj.cinema.common.mapper.MovieActorMapper;
 import com.pmsj.cinema.common.mapper.MovieMapper;
+import com.pmsj.cinema.common.mapper.MovieTpyeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,6 +25,12 @@ public class MovieService {
     @Autowired
     MovieMapper movieMapper;
 
+    @Autowired
+    MovieTpyeMapper movieTpyeMapper;
+
+    @Autowired
+    MovieActorMapper movieActorMapper;
+
 
     public PageInfo<Movie> getAllMovies(Integer page, Integer limit) {
 
@@ -27,5 +38,38 @@ public class MovieService {
         List<Movie> movies = movieMapper.selectAll();
         return new PageInfo<Movie>(movies);
 
+    }
+
+
+    @Transactional
+    public void addMovie(Movie movie, String[] movieTypes, String[] cast0, String[] cast1, String[] cast1Role) {
+
+        movieMapper.insert(movie);
+
+        int movieMapperMaxId = movieMapper.getMaxId();
+
+        //类别关联添加
+        for (String movieType : movieTypes) {
+            movieTpyeMapper.addMovieTypeMid(movieMapperMaxId, Integer.parseInt(movieType));
+        }
+
+        //导演的关联
+        for (String s : cast0) {
+            MovieActor movieActor = new MovieActor();
+            movieActor.setActorId(Integer.parseInt(s));
+            movieActor.setMovieId(movieMapperMaxId);
+            movieActor.setMovieActorType(0);
+            movieActorMapper.insert(movieActor);
+        }
+
+        //演员的关联
+        for (int i = 0; i < cast1.length; i++) {
+            MovieActor movieActor = new MovieActor();
+            movieActor.setActorId(Integer.parseInt(cast1[i]));
+            movieActor.setMovieId(movieMapperMaxId);
+            movieActor.setMovieActorType(1);
+            movieActor.setMovieActorRole("饰：" + cast1Role[i]);
+            movieActorMapper.insert(movieActor);
+        }
     }
 }
