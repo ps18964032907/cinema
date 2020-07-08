59aada737dfb2b9f5c12f52bab027a91b0abed3a
mhd <mhd@qq.com>
2020-07-08 11:26:10 +0800

    xuanzuo

---
+++ src/main/java/com/pmsj/cinema/common/mapper/SeatMapper.java
@@ -0,0 +1,20 @@
+package com.pmsj.cinema.common.mapper;
+
+import com.pmsj.cinema.common.entity.Seat;
+import java.util.List;
+
+public interface SeatMapper {
+    int deleteByPrimaryKey(Integer seatId);
+
+    int insert(Seat record);
+
+    Seat selectByPrimaryKey(Integer seatId);
+
+    List<Seat> selectAll();
+
+    int updateByPrimaryKey(Seat record);
+
+    List<Seat> selectByHallId(Integer hallMovieId);
+
+    List<Seat> selectByHallMovieId(Integer hallMovieId);
+}
